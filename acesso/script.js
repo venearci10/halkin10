48b07418d162baadd05ea5c149afc92312842696
venearci10 <venearci@gmail.com>
2026-07-06 15:33:39 -0400

    Update script.js

--- acesso/script.js
+++ acesso/script.js
@@ -4,7 +4,7 @@ import { getFirestore, doc, getDoc, setDoc, collection, query, where, getDocs } 
 
 // Credenciales de tu proyecto Halkin 10
 const firebaseConfig = {
-    apiKey: "AIzaSyC_QVqEUFopfS29g_jR4vU4oB-K-F-Wq_4", 
+    apiKey: "AIzaSyC_QVqEUFopf4AtMe4Ov9WvG4hCdX4DKNo", 
     authDomain: "halkin10-3868a.firebaseapp.com",
     projectId: "halkin10-3868a",
     storageBucket: "halkin10-3868a.appspot.com",
